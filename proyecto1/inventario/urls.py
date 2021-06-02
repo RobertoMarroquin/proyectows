4f017b18a022592ac7f87309f1ff5ee807944633
Roberto Marroquin <robma96@gmail.com>
2021-06-02 12:33:55 -0600

    urls de inventario

--- proyecto1/inventario/urls.py
+++ proyecto1/inventario/urls.py
@@ -3,22 +3,22 @@ from rest_framework import routers
 from .views import *
 
 router = routers.DefaultRouter()
-router.register(r'usuario', Usuario)
-router.register(r'rol', Rol)
-router.register(r'farmacia', Farmacia)
-router.register(r'tipoDocumento', TipoDocumento)
-router.register(r'proveedor', Proveedor)
-router.register(r'existenciaBodega', ExistenciaBodega)
-router.register(r'producto', Producto)
-router.register(r'unidadDeMedida', UnidadDeMedida)
-router.register(r'tipoProducto', TipoProducto)
-router.register(r'salidaBodega', SalidaBodega)
-router.register(r'catalogoMarca', CatalogoMarca)
-router.register(r'caja', Caja)
-router.register(r'movimiento', Movimiento)
-router.register(r'tipoMovimiento', TipoMovimiento)
+router.register(r'usuario', UsuarioAPI)
+router.register(r'rol', RolAPI)
+router.register(r'farmacia', FarmaciaAPI)
+router.register(r'tipoDocumento', TipoDocumentoAPI)
+router.register(r'proveedor', ProveedorAPI)
+router.register(r'existenciaBodega', ExistenciaBodegaAPI)
+router.register(r'producto', ProductoAPI)
+router.register(r'unidadDeMedida', UnidadDeMedidaAPI)
+router.register(r'tipoProducto', TipoProductoAPI)
+router.register(r'salidaBodega', SalidaBodegaAPI)
+router.register(r'catalogoMarca', CatalogoMarcaAPI)
+router.register(r'caja', CajaAPI)
+router.register(r'movimiento', MovimientoAPI)
+router.register(r'tipoMovimiento', TipoMovimientoAPI)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-inventario/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-inventario/', include('rest_framework.urls', namespace='rest_framework_inventario')),
 ]
--- proyecto1/proyecto1/urls.py
+++ proyecto1/proyecto1/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cafeteria/', include('cafeteria.urls')),
+    path('inventario/', include('inventario.urls')),
 ]
